0dd2e99bd00f4839e1b1a1cd45536a064b5b8ba5
Andrey Semenov <101055766+Smkin32@users.noreply.github.com>
2025-06-06 02:15:37 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,5 +1,9 @@
 import os
 
+os.environ.setdefault('DJANGO_SUPERUSER_USERNAME', 'admin')
+os.environ.setdefault('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+os.environ.setdefault('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
 os.system("pip install -r requirements.txt")
 os.system("python manage.py migrate")
 os.system("python manage.py collectstatic --noinput")
