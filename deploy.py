55296409ee4cb9a89490c38466c1ebd645b1450e
Andrey Semenov <101055766+Smkin32@users.noreply.github.com>
2025-06-06 02:32:16 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,5 +6,5 @@ os.environ.setdefault('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
 os.system("pip install -r requirements.txt")
 os.system("python manage.py migrate")
-os.system("python manage.py collectstatic --noinput")
+os.system("python manage.py collectstatic")
 os.system("python manage.py createsuperuser --noinput")
